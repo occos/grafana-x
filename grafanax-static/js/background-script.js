64611a1554d38add8c6ba0805b87c5e98d51c8b7
tay3223 <tay3223@qq.com>
2021-06-02 13:20:28 +0800

    update

--- grafanax-static/js/background-script.js
+++ grafanax-static/js/background-script.js
@@ -12,152 +12,152 @@ let _isIninting = false;
 let _tmpTestLock = true;
 
 // 在接收端设置一个runtime.onMessage事件监听器来处理消息（请求，发件人，发送响应）
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	// 选区对应的代码块进行执行
-	switch (request.cmd) {
-		case 'alert':
-			console.log('alert:', request.message);
-			alert(request.message);
-			break;
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    // 选区对应的代码块进行执行
+    switch (request.cmd) {
+        case 'alert':
+            console.log('alert:', request.message);
+            alert(request.message);
+            break;
 
 
 
-			//后台进程初始化
-		case 'getBackgroundStatus':
-			// _handaoAlert();
-			break;
+        //后台进程初始化
+        case 'getBackgroundStatus':
+            // _handaoAlert();
+            break;
 
-		default:
-			console.log('未知命令', request.cmd);
-	}
+        default:
+            console.log('未知命令', request.cmd);
+    }
 
-	// 发送响应
-	sendResponse({
-		daemonSwitch: daemonSwitch, //第一次返回的参数是false
-		dashboardId: dashboardId, //第一次返回的编号是0
-		dashboardUrl: dashboardUrl, //第一次返回的RUL是空
-	})
+    // 发送响应
+    sendResponse({
+        daemonSwitch: daemonSwitch, //第一次返回的参数是false
+        dashboardId: dashboardId, //第一次返回的编号是0
+        dashboardUrl: dashboardUrl, //第一次返回的RUL是空
+    })
 });
 
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if (daemonSwitch) {
-		if (dashboardUrl === '' || dashboardId === 0) {
-			console.log('未初始化dashboard页面信息');
-			return;
-		}
-		if ((changeInfo.status === 'complete') && (dashboardUrl === tab.url) && (dashboardId === tabId)) {
-			setTimeoutByRefresh(function() {
-				chrome.tabs.sendMessage(dashboardId, {
-					cmd: 'restartFromBackground'
-				});
-			}, '10s后尝试重新初始化插件');
-		} else if ((dashboardUrl !== tab.url) && (dashboardId === tabId)) {
-			if ('http://grafana.iquanwai.work/login' === tab.url) {
-				if (tryLoginTimes < 5) {
-					setTimeoutByRefresh(function() {
-						chrome.tabs.sendMessage(dashboardId, {
-							cmd: 'login',
-							tryLoginTimes: tryLoginTimes
-						});
-						tryLoginTimes++;
-						console.log('尝试登录次数:', tryLoginTimes);
-					}, '10s后尝试自动登录');
-					return;
-				}
-				setTimeoutByRefresh(function() {
-					alert({
-						title: '登录信息失效，请重新登录',
-						message: '0',
-						voice: true,
-					});
-				}, '登录信息失效，请重新登录');
-				return;
-			}
-			tryLoginTimes = 0;
-			setTimeoutByRefresh(function() {
-				chrome.tabs.update(tabId, {
-					url: dashboardUrl,
-				});
-			}, '10s后尝试重新跳转页面');
-		}
-	}
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (daemonSwitch) {
+        if (dashboardUrl === '' || dashboardId === 0) {
+            console.log('未初始化dashboard页面信息');
+            return;
+        }
+        if ((changeInfo.status === 'complete') && (dashboardUrl === tab.url) && (dashboardId === tabId)) {
+            setTimeoutByRefresh(function () {
+                chrome.tabs.sendMessage(dashboardId, {
+                    cmd: 'restartFromBackground'
+                });
+            }, '10s后尝试重新初始化插件');
+        } else if ((dashboardUrl !== tab.url) && (dashboardId === tabId)) {
+            if ('http://grafana.iquanwai.work/login' === tab.url) {
+                if (tryLoginTimes < 5) {
+                    setTimeoutByRefresh(function () {
+                        chrome.tabs.sendMessage(dashboardId, {
+                            cmd: 'login',
+                            tryLoginTimes: tryLoginTimes
+                        });
+                        tryLoginTimes++;
+                        console.log('尝试登录次数:', tryLoginTimes);
+                    }, '10s后尝试自动登录');
+                    return;
+                }
+                setTimeoutByRefresh(function () {
+                    alert({
+                        title: '登录信息失效，请重新登录',
+                        message: '0',
+                        voice: true,
+                    });
+                }, '登录信息失效，请重新登录');
+                return;
+            }
+            tryLoginTimes = 0;
+            setTimeoutByRefresh(function () {
+                chrome.tabs.update(tabId, {
+                    url: dashboardUrl,
+                });
+            }, '10s后尝试重新跳转页面');
+        }
+    }
 });
 
-chrome.tabs.onRemoved.addListener(function(tabId) {
-	if (dashboardId === tabId) {
-		if (daemonSwitch) {
-			console.log('10s后尝试重新打开dashboard页面');
-			setTimeout(function() {
-				chrome.tabs.create({
-					url: dashboardUrl,
-				}, function(tab) {
-					dashboardId = tab.id;
-				});
-			}, 10000);
-		} else {
+chrome.tabs.onRemoved.addListener(function (tabId) {
+    if (dashboardId === tabId) {
+        if (daemonSwitch) {
+            console.log('10s后尝试重新打开dashboard页面');
+            setTimeout(function () {
+                chrome.tabs.create({
+                    url: dashboardUrl,
+                }, function (tab) {
+                    dashboardId = tab.id;
+                });
+            }, 10000);
+        } else {
 
-			daemonSwitch = false;
-			dashboardId = 0;
-			dashboardUrl = '';
-			console.log('复原后台参数');
-		}
-	}
+            daemonSwitch = false;
+            dashboardId = 0;
+            dashboardUrl = '';
+            console.log('复原后台参数');
+        }
+    }
 });
 
 function alert(message) {
-	chrome.notifications.create(null, {
-		type: 'basic',
-		iconUrl: 'icon.png',
-		title: message.title,
-		message: '报警次数：' + message.message
-	});
+    chrome.notifications.create(null, {
+        type: 'basic',
+        iconUrl: 'icon.png',
+        title: message.title,
+        message: '报警次数：' + message.message
+    });
 
-	if (message.voice === true) {
-		let audio = document.getElementById("alertMp3");
-		audio.volume = 0.5;
-		audio.play();
-		setTimeout(function() {
-			// window.speechSynthesis.cancel();
-			let speechLine = new SpeechSynthesisUtterance(message.title);
-			speechLine.rate = 0.9;
-			window.speechSynthesis.speak(speechLine);
-		}, 2000)
-	}
+    if (message.voice === true) {
+        let audio = document.getElementById("alertMp3");
+        audio.volume = 0.5;
+        audio.play();
+        setTimeout(function () {
+            // window.speechSynthesis.cancel();
+            let speechLine = new SpeechSynthesisUtterance(message.title);
+            speechLine.rate = 0.9;
+            window.speechSynthesis.speak(speechLine);
+        }, 2000)
+    }
 }
 
 function setTimeoutByRefresh(func = {}, logMsg = '') {
-	if (!_isIninting) {
-		console.log(logMsg);
-		setTimeout(function() {
-			func();
-			_isIninting = false;
-		}, 10000);
-		_isIninting = true;
-	}
+    if (!_isIninting) {
+        console.log(logMsg);
+        setTimeout(function () {
+            func();
+            _isIninting = false;
+        }, 10000);
+        _isIninting = true;
+    }
 }
 
 function clock() {
-	let nowHour = (new Date()).getHours();
-	let nowMinute = (new Date()).getMinutes();
-	if (_tmpTestLock && (nowHour === 18) && (nowMinute >= 10)) {
-		_tmpTestAlert();
-		_tmpTestLock = false;
-	} else if (nowHour !== 18) {
-		_tmpTestLock = true;
-	}
-	setTimeout(function() {
-		clock();
-	}, 1000 * 60 * 10);
+    let nowHour = (new Date()).getHours();
+    let nowMinute = (new Date()).getMinutes();
+    if (_tmpTestLock && (nowHour === 18) && (nowMinute >= 10)) {
+        _tmpTestAlert();
+        _tmpTestLock = false;
+    } else if (nowHour !== 18) {
+        _tmpTestLock = true;
+    }
+    setTimeout(function () {
+        clock();
+    }, 1000 * 60 * 10);
 }
 
 function _handaoAlert() {
-	let speechLine = new SpeechSynthesisUtterance("运维自动化报警");
-	speechLine.rate = 0.9;
-	speechLine.volume = 1;
-	window.speechSynthesis.speak(speechLine);
-	speechLine.text = "接口延时异常";
-	speechLine.rate = 0.9;
-	speechLine.volume = 1;
-	window.speechSynthesis.speak(speechLine);
+    let speechLine = new SpeechSynthesisUtterance("运维自动化报警");
+    speechLine.rate = 0.9;
+    speechLine.volume = 1;
+    window.speechSynthesis.speak(speechLine);
+    speechLine.text = "接口延时异常";
+    speechLine.rate = 0.9;
+    speechLine.volume = 1;
+    window.speechSynthesis.speak(speechLine);
 }
--- grafanax-static/js/popup.js
+++ grafanax-static/js/popup.js
@@ -17,123 +17,123 @@ let _waitCount = 0; //等待次数
 
 
 // 通信：扩展程序--->内容脚本
-chrome.tabs.query({active: true,currentWindow: true}, function(tabs) {
-	let reg = /^http:\/\/grafana.iquanwai.work\/d\/.*/;
+chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    let reg = /^http:\/\/grafana.iquanwai.work\/d\/.*/;
 
-	// if (reg.test(tabs[0].url)) {
-	if (true) {
-		dashboardId = tabs[0].id; //获取标签页ID：106
-		dashboardUrl = tabs[0].url; //获取标签页RUL："http://grafana.iquanwai.work/d/4y_eIZeMz/testmian-ban?orgId=1"
+    // if (reg.test(tabs[0].url)) {
+    if (true) {
+        dashboardId = tabs[0].id; //获取标签页ID：106
+        dashboardUrl = tabs[0].url; //获取标签页RUL："http://grafana.iquanwai.work/d/4y_eIZeMz/testmian-ban?orgId=1"
 
-		// 调用该函数获取内容状态，实参为：getPanelObjects
-		getContentStatus('getPanelObjects');
+        // 调用该函数获取内容状态，实参为：getPanelObjects
+        getContentStatus('getPanelObjects');
 
-		// 调用该函数获取后台内容的状态，没有参数
-		getBackgroundStatus();
-		
-		// 初始化面板配置页面
-		init();
-		
-	} else {
-		$init_btn.css('display', 'none');
-		$message_btn.html('不是grafana页面').css('display', 'inline-block');
-	}
+        // 调用该函数获取后台内容的状态，没有参数
+        getBackgroundStatus();
+
+        // 初始化面板配置页面
+        init();
+
+    } else {
+        $init_btn.css('display', 'none');
+        $message_btn.html('不是grafana页面').css('display', 'inline-block');
+    }
 });
-
 
 
 // 获取内容状态----getPanelObjects
 function getContentStatus(cmd = 'getContentStatus', options = {}) {
-	// 向指定标签页中的内容脚本发送一个消息，当发回响应时执行一个可选的回调函数。当前扩展程序在指定标签页中的每一个内容脚本都会收到 runtime.onMessage 事件。
-	chrome.tabs.sendMessage(dashboardId, {cmd: cmd, options: options}, function(message) {
-		contentStatus = message;
-		// 结果如下：
-		// ==========================
-		// isRunning: isRunning, //第一次返回的时候依然还是false
-		// alertMaxNum: alertMaxNum, //第一返回的时候出发次数是7
-		// interval: interval, //第一次返回的时候，时间是10000
-		// voice: voice, //第一次返回的时候，结果是true
-		// panelList: panelList, //第一次返回的时候，是一个panelList.table_panel_container_list里装满new_panel对象的结果集
-		
-		_getContentStatusFinish = true;
-		console.log('contentStatus', contentStatus);
-	});
+    // 向指定标签页中的内容脚本发送一个消息，当发回响应时执行一个可选的回调函数。当前扩展程序在指定标签页中的每一个内容脚本都会收到 runtime.onMessage 事件。
+    chrome.tabs.sendMessage(dashboardId, {cmd: cmd, options: options}, function (message) {
+        contentStatus = message;
+        // 结果如下：
+        // ==========================
+        // isRunning: isRunning, //第一次返回的时候依然还是false
+        // alertMaxNum: alertMaxNum, //第一返回的时候出发次数是7
+        // interval: interval, //第一次返回的时候，时间是10000
+        // voice: voice, //第一次返回的时候，结果是true
+        // panelList: panelList, //第一次返回的时候，是一个panelList.table_panel_container_list里装满new_panel对象的结果集
+
+        _getContentStatusFinish = true;
+        console.log('contentStatus', contentStatus);
+    });
 }
 
 // 获取后台进程状态----没有参数
 function getBackgroundStatus(cmd = 'getBackgroundStatus', options = {}) {
-	// 通信：扩展程序--->后台进程
-	chrome.runtime.sendMessage({cmd: cmd,options: options}, function(message) {
-		backgroundStatus = message;
-		// 结果如下：
-		// ==========================
-		// daemonSwitch: daemonSwitch, //第一次返回的参数是false
-		// dashboardId: dashboardId, //第一次返回的编号是0
-		// dashboardUrl: dashboardUrl, //第一次返回的RUL是空
-		
-		_getBackgroundStatusFinish = true;
-		console.log('backgroundStatus', backgroundStatus);
-	});
+    // 通信：扩展程序--->后台进程
+    chrome.runtime.sendMessage({cmd: cmd, options: options}, function (message) {
+        backgroundStatus = message;
+        // 结果如下：
+        // ==========================
+        // daemonSwitch: daemonSwitch, //第一次返回的参数是false
+        // dashboardId: dashboardId, //第一次返回的编号是0
+        // dashboardUrl: dashboardUrl, //第一次返回的RUL是空
+
+        _getBackgroundStatusFinish = true;
+        console.log('backgroundStatus', backgroundStatus);
+    });
 }
 
 // 初始化函数
 function init() {
-	// 这两个变量在getContentStatus()与getBackgroundStatus()函数中会被改写为true；
-	if (_getBackgroundStatusFinish && _getContentStatusFinish) {
-		if (contentStatus.isRunning) {
-			$init_btn.css('display', 'none');
-			$restart_btn.css('display', 'inline-block');
-			popupWindow();
-		} else if (backgroundStatus.daemonSwitch) {
-			$init_btn.css('display', 'none');
-			let message = '正在尝试重启';
-			if (backgroundStatus.dashboardId !== dashboardId) {
-				message = '目前只支持单个dashboard';
-			}
-			$message_btn.html(message).css('display', 'inline-block');
-		} else {
-			console.log("如果守护进程也没开，就什么都不做");
-		}
-	} else if (_waitCount < 5) {
-		console.log('等待次数:', _waitCount++);
-		setTimeout(function() {
-			init();
-		}, 200);
-	}
+    // 这两个变量在getContentStatus()与getBackgroundStatus()函数中会被改写为true；
+    if (_getBackgroundStatusFinish && _getContentStatusFinish) {
+        if (contentStatus.isRunning) {
+            $init_btn.css('display', 'none');
+            $restart_btn.css('display', 'inline-block');
+            popupWindow();
+        } else if (backgroundStatus.daemonSwitch) {
+            $init_btn.css('display', 'none');
+            let message = '正在尝试重启';
+            if (backgroundStatus.dashboardId !== dashboardId) {
+                message = '目前只支持单个dashboard';
+            }
+            $message_btn.html(message).css('display', 'inline-block');
+        } else {
+            console.log("如果守护进程也没开，就什么都不做");
+        }
+    } else if (_waitCount < 5) {
+        console.log('等待次数:', _waitCount++);
+        setTimeout(function () {
+            init();
+        }, 200);
+    }
 }
 
 // 点击初始化监控
-$init_btn.click(function() {
-	popupWindow();
-	$(this).css('display', 'none'); //初始化监控按钮隐藏
-	$start_btn.css('display', 'inline-block'); //开始运行按钮显示为块
+$init_btn.click(function () {
+    popupWindow();
+    $(this).css('display', 'none'); //初始化监控按钮隐藏
+    $start_btn.css('display', 'inline-block'); //开始运行按钮显示为块
 });
 
 // 弹出窗口（显示运行之前的设置面板）
 function popupWindow() {
-	$('#interval').val(contentStatus.interval); //默认值是10000毫秒，直接设置到DOM中
-	$('#limit').val(contentStatus.alertMaxNum); //触发次数，默认值是1，写入到报警触发边界中
-	$('#voice').attr("checked", contentStatus.voice); //语音开关是true，直接设置到DOM中
-	$('#daemonSwitch').attr("checked", backgroundStatus.daemonSwitch); //守护进程开关默认是false，直接设置到DOM中
+    $('#interval').val(contentStatus.interval); //默认值是10000毫秒，直接设置到DOM中
+    $('#limit').val(contentStatus.alertMaxNum); //触发次数，默认值是1，写入到报警触发边界中
+    $('#voice').attr("checked", contentStatus.voice); //语音开关是true，直接设置到DOM中
+    $('#daemonSwitch').attr("checked", backgroundStatus.daemonSwitch); //守护进程开关默认是false，直接设置到DOM中
 
-	let tableList = '';
-	console.log("444--------------------------");
-	console.log(contentStatus.panelList);
-	for (let key in contentStatus.panelList) { //内容状态中给出的返回值里包含panelList
-		console.log(key);
-		tableList += _tableContent(contentStatus.panelList[key], key);
-	}
-	$("body").css("width", "500px");
-	$("#tableList").html(tableList);
-	$("#form").css('display', 'inline');
+    let tableList = '';
+    console.log("444--------------------------");
+    console.log(contentStatus.panelList);
+    for (let key in contentStatus.panelList) { //内容状态中给出的返回值里包含panelList
+        console.log(key);
+        tableList += _tableContent(contentStatus.panelList[key], key);
+    }
+    $("body").css("width", "500px");
+    $("#tableList").html(tableList);
+    $("#form").css('display', 'inline');
 }
+
 // 弹出窗口运行，到此结束，剩下的动作就需要其它按钮来触发了
 
 
 // 形成表格
 function _tableContent(panelList, type) {
-	let length = panelList.length;
-	let table = `<table id="#` + type + `" class="table table-striped">
+    let length = panelList.length;
+    let table = `<table id="#` + type + `" class="table table-striped">
         <thead>
         <tr>
             <th scope="col">#</th>
@@ -142,20 +142,20 @@ function _tableContent(panelList, type) {
         </tr>
         </thead>
         <tbody id="panel_info_` + type + `">`;
-	for (let i = 0; i < length; i++) {
-		if (panelList[i] === null) {
-			continue;
-		}
-		table += _trContent(i, panelList[i].title, panelList[i].defaultValue, type);
-	}
-	table += `</tbody></table>`;
+    for (let i = 0; i < length; i++) {
+        if (panelList[i] === null) {
+            continue;
+        }
+        table += _trContent(i, panelList[i].title, panelList[i].defaultValue, type);
+    }
+    table += `</tbody></table>`;
 
-	return table;
+    return table;
 }
 
 // 形成表格
 function _trContent(tableNum, title, value, type) {
-	return `<tr>
+    return `<tr>
             <td>
                 <input type="checkbox" id="panel_` + type + tableNum + `" checked>
             </td>
@@ -172,53 +172,51 @@ function _trContent(tableNum, title, value, type) {
 
 
 // 确认并开始监控
-$start_btn.click(function() {
-	// ID异常时，强制终止运行
-	if (dashboardId === 0) {
-		console.log('dashboard页面异常');
-		return;
-	}
-	let options = getOptions(); //得到一个字典对象
-	getContentStatus('start', options); //发起一个后端通讯，这一次携带的参数不一样
-	getBackgroundStatus('setBackgroundStatus', options);
-	$(this).css('display', 'none');
-	$restart_btn.css('display', 'inline-block');
+$start_btn.click(function () {
+    // ID异常时，强制终止运行
+    if (dashboardId === 0) {
+        console.log('dashboard页面异常');
+        return;
+    }
+    let options = getOptions(); //得到一个字典对象
+    getContentStatus('start', options); //发起一个后端通讯，这一次携带的参数不一样
+    getBackgroundStatus('setBackgroundStatus', options);
+    $(this).css('display', 'none');
+    $restart_btn.css('display', 'inline-block');
 });
 
-$restart_btn.click(function() {
-	let options = getOptions();
-	getContentStatus('restart', options);
-	getBackgroundStatus('setBackgroundStatus', options);
+$restart_btn.click(function () {
+    let options = getOptions();
+    getContentStatus('restart', options);
+    getBackgroundStatus('setBackgroundStatus', options);
 });
-
-
 
 
 // 获取运行参数，这些参数都是之前各个环境中拼接好的，直接获取即可
 function getOptions() {
-	let options = {}; //声明一个空的字典
-	options.interval = $('#interval').val(); //间隔时间
-	options.alertMaxNum = $('#limit').val(); //触发边界
-	options.voice = $('#voice').is(':checked'); //语音开关
-	options.daemonSwitch = $('#daemonSwitch').is(':checked'); //后台开关
-	options.dashboardId = dashboardId; //仪表盘ID
-	options.dashboardUrl = dashboardUrl; //仪表盘URL
-	_setPanelListValue(); 
-	options.panelList = panelList; //面板集合
-	return options; //返回一个字典对象
+    let options = {}; //声明一个空的字典
+    options.interval = $('#interval').val(); //间隔时间
+    options.alertMaxNum = $('#limit').val(); //触发边界
+    options.voice = $('#voice').is(':checked'); //语音开关
+    options.daemonSwitch = $('#daemonSwitch').is(':checked'); //后台开关
+    options.dashboardId = dashboardId; //仪表盘ID
+    options.dashboardUrl = dashboardUrl; //仪表盘URL
+    _setPanelListValue();
+    options.panelList = panelList; //面板集合
+    return options; //返回一个字典对象
 }
 
 // 面板详情数据（list数据）
 function _setPanelListValue() {
-	for (let key in panelList) { //遍历面板集合
-		if (panelList.hasOwnProperty(key)) { //判断面板集合中是否有指定的键
-			let length = panelList[key].length; //获取单个面板的长度
-			for (let i = 0; i < length; i++) { //遍历面板的子集对象
-				panelList[key][i] = $("#panel_value_" + key + i).val(); //给文本框里赋值
-				if ($("#panel_" + key + i).is(':checked') === false) {
-					panelList[key][i] = null;
-				}
-			}
-		}
-	}
+    for (let key in panelList) { //遍历面板集合
+        if (panelList.hasOwnProperty(key)) { //判断面板集合中是否有指定的键
+            let length = panelList[key].length; //获取单个面板的长度
+            for (let i = 0; i < length; i++) { //遍历面板的子集对象
+                panelList[key][i] = $("#panel_value_" + key + i).val(); //给文本框里赋值
+                if ($("#panel_" + key + i).is(':checked') === false) {
+                    panelList[key][i] = null;
+                }
+            }
+        }
+    }
 }
